e6cffb284e1662ebf1d344157b528ae560ce8c1b
William Keller <will.keller98@gmail.com>
2022-10-30 10:37:19 -0500

    builing mlp_0 model

---
+++ src/models/mlp_0.py
@@ -0,0 +1,79 @@
+from random import shuffle
+from xml.etree.ElementPath import prepare_descendant
+import torch
+from torch import nn 
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from sklearn.metrics import confusion_matrix
+
+if torch.cuda.is_available():
+    device=torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+print('Pytorch Version: ', torch.__version__, ' Device: ', device)
+
+mlp_0 = nn.Sequential(
+    nn.Linear(784, 1000),
+    nn.ReLU(),
+    nn.Linear(1000, 10),
+    nn.LogSoftmax(dim=1)
+)
+
+optimizer = torch.optim.SGD(mlp_0.parameters(), lr=0.01)
+criterion = nn.NLLLoss()
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data/mnist', train=True, download=True, transform=transforms.Compose([
+        transforms.ToTensor(), transforms.Normalize((0.1307,),(0.3081,))
+    ])), batch_size=128, shuffle=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data/mnist', train=False, download=True, transform=transforms.Compose([
+        transforms.ToTensor(), transforms.Normalize((0.1307,),(0.3081,))
+    ])), batch_size=128, shuffle=True
+)
+
+def train(epoch):
+    mlp_0.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data = data.view(-1, 784)
+        optimizer.zero_grad()
+        output = mlp_0(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} '+
+                  f'({(100.*batch_idx/len(train_loader)):.2f}%)]\tLoss: {loss.item():.6f}')
+                  
+def test(model):
+    y_pred=[]
+    y_true=[]
+    model.eval()
+    test_loss = 0
+    correct=0
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data=data.view(-1,784)
+            output=model(data)
+            test_loss+=criterion(output,target).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct+= pred.eq(target.data.view_as(pred)).sum()
+
+            output = (torch.max(torch.exp(output),1)[1]).data.cpu().numpy()
+            y_pred.extend(output)
+            target = target.data.cpu().numpy()
+            y_true.extend(target)
+    
+    test_loss/=len(test_loader.dataset)
+    print(f'Test Set: Avg. Loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} ({(100.*correct/len(test_loader.dataset)):.2f}%) ')
+    conf_mat = confusion_matrix(y_pred, y_true)
+    print(conf_mat)
+
+if __name__ == '__main__':
+    for epoch in range(1,4):
+        train(epoch)
+    test(mlp_0)
